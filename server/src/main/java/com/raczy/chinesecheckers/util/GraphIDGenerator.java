7c55438d8a282026c0ba38e9aa337ca2068b4eba
Kacper Raczy <gfw.kra@gmail.com>
2017-12-16 23:30:51 +0100

    Added id generator for graph-nodes.

---
+++ server/src/main/java/com/raczy/chinesecheckers/util/GraphIDGenerator.java
@@ -0,0 +1,17 @@
+package com.raczy.chinesecheckers.util;
+
+/**
+ * Created by kacperraczy on 11.12.2017.
+ */
+public class GraphIDGenerator {
+    private int current;
+
+    public GraphIDGenerator() {
+        this.current = -1;
+    }
+
+    public int generate() {
+        current+=1;
+        return current;
+    }
+}
